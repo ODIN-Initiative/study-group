f730b7a4f40e924d0f920a7d074b71919ed94b7d
Newman <nslanier@yahoo.com>
2025-06-13 22:53:25 -0400

    chore: update dependencies in package.json for improved performance and stability
    docs: add new presentation details for Q1 2025 and update existing entries in presentations2025q1.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,35 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://odin-initiative.github.io',
-	base: '/study-group',
+	site: "https://odin-initiative.github.io",
+	base: "/study-group",
 	integrations: [
 		starlight({
-			title: 'ODIN Study Group',
-			social: {
-				github: 'https://github.com/ODIN-Initiative',
-				twitter: 'https://x.com/odininitiative',
-				youtube: 'https://www.youtube.com/@odininitiative',
-				discord: 'https://discord.gg/FVRjwdHR'
-			},
+			title: "ODIN Study Group",
+			social: [
+				{ icon: "twitter", label: "x", href: "https://x.com/odininitiative" },
+				{ icon: "youtube", label: "Youtube", href: "https://www.youtube.com/@odininitiative" },
+				{ icon: "discord", label: "Discord", href: "https://discord.gg/FVRjwdHR" },
+				{ icon: "github", label: "Github", href: "https://github.com/ODIN-Initiative" },
+			],
 			sidebar: [
 				{
-					label: 'Presentations',
-					autogenerate: { directory: 'presentations' },
+					label: "Presentations",
+					autogenerate: { directory: "presentations" },
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
 			customCss: [
 				// Relative path to your custom CSS file
-				'./src/styles/custom.css',
+				"./src/styles/custom.css",
 			  ],
 		}),
 	],
+	output: "static"
 });
